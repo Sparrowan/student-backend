c92724fda179ac7b427733fa9b770e53626afc0a
Alphius <alphiuswambua@gmail.com>
2025-08-20 17:07:52 +0300

    Fixed loading time

--- src/main/java/com/example/studentbackend/StudentBackendApplication.java
+++ src/main/java/com/example/studentbackend/StudentBackendApplication.java
@@ -2,10 +2,22 @@ package com.example.studentbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class StudentBackendApplication {
+public class StudentBackendApplication implements WebMvcConfigurer {
+
     public static void main(String[] args) {
         SpringApplication.run(StudentBackendApplication.class, args);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")          // allow all endpoints
+                .allowedOrigins("http://localhost:4200") // allow your Angular app
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
+
